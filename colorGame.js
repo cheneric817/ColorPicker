830c353767ec8ced9b61068029c5b8335d65632d
Eric Chen <cheneric817@gmail.com>
2019-10-17 14:53:14 +0800

    Added changeColors() function. Rearranged codes to make it readable.

--- colorGame.js
+++ colorGame.js
@@ -10,30 +10,42 @@ var colors = [
 
 //select the squares
 var squares = document.querySelectorAll(".square");
-
 //set a picked color to be the forth color in the array
 var pickedColor = colors[3];
-
-//select the <span> colorDisplay
+//select the <span> "colorDisplay" from the HTML doc
 var colorDisplay = document.getElementById("colorDisplay");
+//select the <span> "message" from the HTML doc
+var messageDisplay = document.querySelector("#message");
 
 //make the h1 topic display the RGB data of pickedColor
 colorDisplay.textContent = pickedColor;
-
 //assign the squares different colors
 for(i = 0; i < squares.length ; i++) {
   //add initial colors to squares
   squares[i].style.background = colors[i];
-
   //add click listeners to squares
   squares[i].addEventListener("click", function() {
     //get color of clicked square
     var clickedColor = (this.style.background);
     //compare color to pickedColor
     if(clickedColor === pickedColor) {
-      alert("correct");
+      //display correct message
+      messageDisplay.textContent = "Correct!";
+      changeColors(clickedColor);
     } else {
-      alert("wrong");
+      //if clicked on the wrong one, it fades out to the background
+      this.style.background = "#232323";
+      //display messageDisplay when gussed wrong
+      messageDisplay.textContent = "Try Again";
     }
-  })
+  });
+}
+
+//create a function that make all squares changes to the correct color when the guessing is correct
+function changeColors(color) {
+  //loop through all squares
+  for(i = 0; i < squares.length; i++) {
+    //change each color to match given color
+    squares[i].style.background = color;
+  }
 }

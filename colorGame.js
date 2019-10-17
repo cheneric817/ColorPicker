8d0d70f870ea34bc855919a0debaf0284c7582b0
Eric Chen <cheneric817@gmail.com>
2019-10-17 13:20:42 +0800

    Assigned basic logic behind clicking on each square. So far the array is hardcoded.

--- colorGame.js
+++ colorGame.js
@@ -10,7 +10,30 @@ var colors = [
 
 //select the squares
 var squares = document.querySelectorAll(".square");
+
+//set a picked color to be the forth color in the array
+var pickedColor = colors[3];
+
+//select the <span> colorDisplay
+var colorDisplay = document.getElementById("colorDisplay");
+
+//make the h1 topic display the RGB data of pickedColor
+colorDisplay.textContent = pickedColor;
+
 //assign the squares different colors
 for(i = 0; i < squares.length ; i++) {
+  //add initial colors to squares
   squares[i].style.background = colors[i];
+
+  //add click listeners to squares
+  squares[i].addEventListener("click", function() {
+    //get color of clicked square
+    var clickedColor = (this.style.background);
+    //compare color to pickedColor
+    if(clickedColor === pickedColor) {
+      alert("correct");
+    } else {
+      alert("wrong");
+    }
+  })
 }
